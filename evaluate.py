77474203a9f7560df73ce917b205f7f1dcf7d85c
szk_t <suzuki_tk4@eis.hokudai.ac.jp>
2020-02-26 12:11:54 +0900

    データセット修正

--- evaluate.py
+++ evaluate.py
@@ -1,32 +1,18 @@
-import pickle
-import time
-from collections import deque
-
 from sklearn.utils import shuffle
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import roc_auc_score
 
 import numpy as np
-import networkx as nx
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-import matplotlib.pyplot as plt
-from multiprocessing import Pool
-
-from scipy.sparse import lil_matrix, csr_matrix, csc_matrix
-from scipy.sparse import vstack
-
-import sys
-
 torch.manual_seed(1)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = 'cpu'
-
 
 class Evalueter:
     #def __init__(self, test_data, user_size, metric):
